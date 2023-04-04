b40eeac56774ad0ff66302cde225a2ea8a066c56
medina <damirovamedina38@gmail.com>
2023-04-04 16:04:48 +0300

    added new nav in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
       
+      <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      </nav>
     </div>
   );
 }
