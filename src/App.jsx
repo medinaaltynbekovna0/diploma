df055a98aad6dc82b3abcff87cf80d5c6c9c975b
medina <damirovamedina38@gmail.com>
2023-04-04 15:50:00 +0300

    removed the tags in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,7 @@ export default function App() {
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
-      <p>hello world</p>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque veniam
-          molestias molestiae reiciendis facilis saepe, cum maxime, in ea atque
-          accusantium architecto a dicta porro ipsum sunt incidunt repellendus
-          vero.
-        </p>
-        <h2>test</h2>
-      </div>
+      
     </div>
   );
 }
