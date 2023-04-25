ebce3d4bc7c9cd540b5821bc35e59f368049518e
medina <damirovamedina38@gmail.com>
2023-04-25 15:41:06 +0300

    renamed the words on new words

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,17 +1,28 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AppContext } from "../../App";
 import "./ProductList.css";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+import AddToCart from "../AddToCart/AddToCart";
 
-export default function ProductList() {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.map((product) => (
+  const output = products
+  .filter(product => product.category === category.id)
+  .map(product => (
     <div className="Product" key={product.id}>
       <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.path}>{product.name}</Link>
+      <Link to={"/product/" + product.path}>
+        {product.name}
+      </Link>
       <span>{product.price} som</span>
+      <AddToCart product={product}/>
     </div>
   ));
-  return <div className="Productlist">{output}</div>;
+
+  return (
+    <div className="ProductList">
+      {output}
+    </div>
+  );
 }
