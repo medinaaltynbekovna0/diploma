74c811c4ec94064a45f7b193042d8aedb9a942e5
medina <damirovamedina38@gmail.com>
2023-05-03 15:50:35 +0300

    added a new components :Layout(jsx,css)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,17 +2,28 @@ import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import NavToggle from "../NavToggle/NavToggle";
 import "./Layout.css";
 import Auth from "../Auth/Auth";
+import Drawer from "../Drawer/Drawer";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink />
         <Auth />
+      
+
       </header>
       <aside>
         <CategoryList />
