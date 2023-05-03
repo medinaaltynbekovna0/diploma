ab78cc05e5b401f349381cb6859bf1ef778fbbdd
medina <damirovamedina38@gmail.com>
2023-05-03 14:12:12 +0300

    imported a new tags

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,20 +1,25 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 import "./CategoryList.css";
+import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
 
   const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+      <NavLink to={"/category/" + category.path}>{category.name}
+      </NavLink>
+      <DeleteCategory category={category} />
     </li>
   ));
 
   return (
     <div className="CategoryList">
       <ul>{output}</ul>
+      <AddCategory />
     </div>
   );
 }
