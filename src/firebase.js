2cb6247f23dc5325b5e9f15176f99ca32c5a64e4
medina <damirovamedina38@gmail.com>
2023-05-03 14:08:41 +0300

    added  new tags in inside firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { collection, getFirestore } from "firebase/firestore";
-import {GoogleAuthProvider, getAuth,  signOut, signInWithPopup, onAuthStateChanged} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { collection, getFirestore, onSnapshot } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,16 +15,43 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); 
-const auth= getAuth(app);
+export const db = getFirestore(app);
+const auth = getAuth(app);
 
-
-//получить список категорий (коллекция документов).
+// получить список категорий (коллекция документов).
 export const categoryCollection = collection(db, 'categories');
-export const productCollection = collection(db,'products');
+export const productCollection = collection(db, 'products');
 export const orderCollection = collection(db, 'orders');
 
 const provider = new GoogleAuthProvider();
 export const logIn = () => signInWithPopup(auth, provider);
 export const logOut = () => signOut(auth);
 export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+
+export const onCategoriesLoad = (callback) =>
+  onSnapshot(categoryCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onProductsLoad = (callback) =>
+  onSnapshot(productCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onOrdersLoad = (callback) =>
+  onSnapshot(orderCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
