186aad63a36049a77c9e4d49a002ae8f6607fcea
medina <damirovamedina38@gmail.com>
2023-05-03 14:51:19 +0300

    imported a new product

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,6 +3,7 @@ import "./ProductList.css";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
+import AddProduct from "../AddProduct/AddProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
@@ -23,6 +24,7 @@ export default function ProductList({ category }) {
   return (
     <div className="ProductList">
       {output}
+      {AddProduct}
     </div>
   );
 }
