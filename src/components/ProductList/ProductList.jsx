1f178f7dd036dcfedaae84d64558b9969a56f16e
medina <damirovamedina38@gmail.com>
2023-05-11 22:14:47 +0300

    added a new components DeleteProduct

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,6 +4,7 @@ import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
+import DeleteProduct from "../DeleteProduct/DeleteProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
@@ -18,13 +19,14 @@ export default function ProductList({ category }) {
       </Link>
       <span>{product.price} som</span>
       <AddToCart product={product}/>
+      <DeleteProduct product={product} />
     </div>
   ));
 
   return (
     <div className="ProductList">
       {output}
-      {AddProduct}
+      <AddProduct category={category} />
     </div>
   );
 }
