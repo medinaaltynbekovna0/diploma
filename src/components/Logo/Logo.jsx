cc1e8f0bf54cdc2fa9381704ef15122cd850a1de
medina <damirovamedina38@gmail.com>
2023-05-12 15:09:02 +0300

    imported a new logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import "./Logo.css";
-import logo from "../../assets/Logo.svg";
+import { NavLink } from "react-router-dom";
 
 
 export default function Logo(){
 	return(
 		<div className="Logo">
+			<NavLink to="Logo">
 			<img src="https://i.pinimg.com/564x/88/49/af/8849af8414cf241c9e0539ab7e0ce2bd.jp" alt="website logo" />
+			</NavLink>
 		</div>
 	);
 }
