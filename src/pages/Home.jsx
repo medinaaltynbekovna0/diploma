5b6268bd3012090a2018bab5b81a8413757bcf40
medina <damirovamedina38@gmail.com>
2023-05-12 16:49:10 +0300

    added new components Hom(jsx,css)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
+import  "./Home.css";
+<Home></Home>
 export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Perfume shop</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-        consequuntur qui et. Sequi et quae quod eveniet corporis neque deserunt
-        voluptate pariatur, itaque distinctio nesciunt ipsum tenetur quisquam at
-        magnam.
       </p>
     </div>
   );
