e9f6089df746b265ef9cb67470fa5abac28a8557
medina <damirovamedina38@gmail.com>
2023-05-12 15:06:01 +0300

    changed logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import logo from "../../assets/Logo.svg";
 export default function Logo(){
 	return(
 		<div className="Logo">
-			<img src={logo} alt="https://i.pinimg.com/564x/88/49/af/8849af8414cf241c9e0539ab7e0ce2bd.jpg" />
+			<img src="https://i.pinimg.com/564x/88/49/af/8849af8414cf241c9e0539ab7e0ce2bd.jp" alt="website logo" />
 		</div>
 	);
 }
