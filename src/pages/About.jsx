da510bc23299698cba735cc4e68a438b2b872a63
medina <damirovamedina38@gmail.com>
2023-05-16 15:36:09 +0300

    added a video

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,11 @@ export default function About() {
       <p>
         People choose scents not only according to the composition by perfumers.They want to create an image and feel different.Passionate,elegant,light,romantic or domineering-all this is possible withthe help of properly selected fruit and floral notes.
       </p>
+      <video controls autoPlay="true" loop="true">
+        <source 
+        src="https://player.vimeo.com/external/546451001.sd.mp4?s=34bb9c207e510aa8180e89ad5272bb3d189dec10&profile_id=165&oauth2_token_id=57447761" />
+      
+      </video>
     </div>
   );
 }
