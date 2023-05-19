356cac1695d8b54bfacb497baec2d0416c1e3e56
medina <damirovamedina38@gmail.com>
2023-05-19 14:25:01 +0300

    changed logo img

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo(){
 	return(
 		<div className="Logo">
 			<NavLink to="/">
-			<img src="https://i.pinimg.com/564x/5d/80/7a/5d807a5711158636f69a639026f25408.jpg" alt="" />
+			<img src="https://img.freepik.com/free-vector/perfume-shop-logo-template-beauty-business-branding-design-black-and-white-vector_53876-156447.jpg?w=2000" alt="" />
 			</NavLink>
 		</div>
 	);
