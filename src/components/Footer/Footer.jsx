830bc4fde4d233fbd5a3de1c25c54ec09d98bec1
medina <damirovamedina38@gmail.com>
2023-05-19 14:22:52 +0300

    added footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import "./Footer.css";
 function Footer() {
 return (
-  <div className="footer">
-		<img src="https://i.pinimg.com/564x/a4/15/70/a415705b2003ddc3c3ef899aedbeff70.jpg"></img>
+  <div className="footer">  
+  <div>Footer</div>
   </div>
 );}
-export default Footer;
+export default Footer;  
