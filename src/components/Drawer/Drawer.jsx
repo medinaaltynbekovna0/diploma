f7fd359948bff2154f15f7b3980ae96c8919f454
medina <damirovamedina38@gmail.com>
2023-05-24 14:47:47 +0300

    adde new code

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -4,7 +4,13 @@ import "./Drawer.css"
 
 function Drawer({ open, toggle }) {
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
-
+  let menuBtn = document.querySelectorAll('.Nav a')
+  let menu = document.querySelector('.Drawer')
+  menuBtn.forEach(el => {
+      el.addEventListener('click', function(){
+          menu.classList.remove('open')
+      })
+  })
   return (
     <div className={drawerClassNames}> 
       <div onClick={toggle} className="backdrop"></div>
