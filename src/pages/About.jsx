f9b6ef0d99b1d037a9398f30d7bf8c3e49d93699
medina <damirovamedina38@gmail.com>
2023-05-24 14:51:47 +0300

    deleted excess code

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,33 @@
-import "./About.css"
+import "./About.css";
 export default function About() {
   return (
     <div className="About">
       <h1>Perfume is the best thing</h1>
-      <p>
-        People choose scents not only according to the composition by perfumers.They want to create an image and feel different.Passionate,elegant,light,romantic or domineering-all this is possible withthe help of properly selected fruit and floral notes.
-      </p>
-      <div className="image">
-      </div>
+
+      <div className="image"></div>
       <div className="about">
-      <img src="https://i.pinimg.com/564x/80/8d/7f/808d7f8af18f2b61bcc6e7267a8c55d7.jpg"></img>
+        {/*<img src="https://i.pinimg.com/564x/80/8d/7f/808d7f8af18f2b61bcc6e7267a8c55d7.jpg"></img>*/}
+      </div>
+
+      <div class="row">
+        <div className="col-6">
+          <div className="about">
+            {/*<img src="https://i.pinimg.com/236x/1b/92/e1/1b92e1420ff012479a458810493739cd.jpg"></img>*/}
+            <img src="https://i.pinimg.com/564x/80/8d/7f/808d7f8af18f2b61bcc6e7267a8c55d7.jpg"></img>
+          </div>
+          <div className="col-6">
+            <p>
+              Cinéma by Yves Saint Laurent is an Oriental Floral fragrance for
+              women. Cinema came out in 2004. Perfumer: Jacques Cavalier. Top
+              notes are almond blossom, clementine and cyclamen; middle notes
+              are jasmine, peony and amaryllis; base notes are vanilla, amber,
+              benzoin and white musk.
+            </p>
         </div>
-      <div className="about">
-      <img src="https://i.pinimg.com/236x/1b/92/e1/1b92e1420ff012479a458810493739cd.jpg"></img>
-      <img src=""></img>
+            <img src=""></img>
+          </div>
+        </div>
       </div>
-              
-      
-    </div>
+
   );
 }
