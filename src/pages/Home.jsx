761ac43253d9e8f946d2319fd05c283a02ae9e19
medina <damirovamedina38@gmail.com>
2023-05-24 14:50:05 +0300

    deleted excess code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Perfume shop</h1>
-      <video controls autoPlay="true" loop="true">
+      <video  autoPlay="true" loop="true">
         <source 
         src="https://player.vimeo.com/external/374819500.sd.mp4?s=3ff467c214bfb8caf9b6a37c98f4925486273fda&profile_id=164&oauth2_token_id=57447761" />
       
