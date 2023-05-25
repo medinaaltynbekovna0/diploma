e6bd4c44358d96b25a772fa6b72ae5ba992720a9
medina <damirovamedina38@gmail.com>
2023-05-25 15:11:15 +0300

    fixed the class error on the className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,16 +8,16 @@ export default function Home() {
         <source 
         src="https://player.vimeo.com/external/374819500.sd.mp4?s=3ff467c214bfb8caf9b6a37c98f4925486273fda&profile_id=164&oauth2_token_id=57447761" />
       </video>
-      <section class="page-section cta">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-9 mx-auto">
-                        <div class="cta-inner bg-faded text-center rounded">
-                            <h2 class="section-heading mb-4">
-                                <span class="section-heading-upper">What is perfumery for?</span>
-                                <span class="section-heading-lower"></span>
+      <section className="page-section cta">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-9 mx-auto">
+                        <div className="cta-inner bg-faded text-center rounded">
+                            <h2 className="section-heading mb-4">
+                                <span className="section-heading-upper">What is perfumery for?</span>
+                                <span className="section-heading-lower"></span>
                             </h2>
-             <p class="mb-0">When we put on a fragrance,it starts to work in two directions-it becomes a kind of signal that tells others information about our mood; becomes a factor that raises self-esteem, makes you feel more attractive</p>
+             <p className="mb-0">When we put on a fragrance,it starts to work in two directions-it becomes a kind of signal that tells others information about our mood; becomes a factor that raises self-esteem, makes you feel more attractive</p>
                         </div>
                     </div>
                   </div>
