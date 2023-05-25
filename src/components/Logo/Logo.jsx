785e2d0f56c6862e3ebef2c353bd1d176a3587b1
medina <damirovamedina38@gmail.com>
2023-05-25 22:19:23 +0300

    commented and added new  in logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -7,6 +7,7 @@ export default function Logo(){
 		<div className="Logo">
 			<NavLink to="/">
 			<img src="https://thumbs.dreamstime.com/t/doodle-sketch-perfume-bottle-illustration-aroma-bottle-icon-doodle-sketch-perfume-bottle-illustration-aroma-retro-bottle-163032466.jpg"alt="" />
+			{/*<img src="https://img.freepik.com/premium-vector/perfume-shop-logo-template_338221-14.jpg"></img>*/}
 			</NavLink>
 		</div>
 	);
