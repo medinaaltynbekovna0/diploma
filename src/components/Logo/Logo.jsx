ab37c8be150bd92323361c24a585128413638720
medina <damirovamedina38@gmail.com>
2023-05-26 13:44:17 +0300

    commented img and added new img in logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -8,6 +8,7 @@ export default function Logo(){
 			<NavLink to="/">
 			<img src="https://thumbs.dreamstime.com/t/doodle-sketch-perfume-bottle-illustration-aroma-bottle-icon-doodle-sketch-perfume-bottle-illustration-aroma-retro-bottle-163032466.jpg"alt="" />
 			{/*<img src="https://img.freepik.com/premium-vector/perfume-shop-logo-template_338221-14.jpg"></img>*/}
+			{/*<img src="https://png.pngtree.com/png-vector/20211011/ourmid/pngtree-perfume-logo-png-image_3979273.png" alt="" />*/}
 			</NavLink>
 		</div>
 	);
