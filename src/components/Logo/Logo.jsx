90436a4c0f9ab44716c09110d99bb3795add846d
medina <damirovamedina38@gmail.com>
2023-05-26 16:26:18 +0300

    added logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,14 @@
 import "./Logo.css";
 import { NavLink } from "react-router-dom";
+import logoo from "../../assets/logo.jpg";
 
 
 export default function Logo(){
 	return(
 		<div className="Logo">
 			<NavLink to="/">
-			<img src="https://thumbs.dreamstime.com/t/doodle-sketch-perfume-bottle-illustration-aroma-bottle-icon-doodle-sketch-perfume-bottle-illustration-aroma-retro-bottle-163032466.jpg"alt="" />
-			{/*<img src="https://img.freepik.com/premium-vector/perfume-shop-logo-template_338221-14.jpg"></img>*/}
+			<img src={logoo} alt="" />
+
 			{/*<img src="https://png.pngtree.com/png-vector/20211011/ourmid/pngtree-perfume-logo-png-image_3979273.png" alt="" />*/}
 			</NavLink>
 		</div>
