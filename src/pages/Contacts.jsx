57085b519d9c86c4a4eb1d73b97d940210ed1ed4
medina <damirovamedina38@gmail.com>
2023-06-06 20:05:11 +0300

    added div with class name contacts_map and iframe src

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,6 +2,8 @@
 export default function Contacts() {
   return (
     <div className="Contacts">
+    <div class="contacts_map"><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Acf98b60208e9449827ed9f5c36c0ffdc0045f73467c474bfe98853fc49bd9729&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe></div>
+    
       <h1>Here is my Contacts</h1>
       <p>+996 504 409 595</p>
       <p>+996 704 881 099</p>
