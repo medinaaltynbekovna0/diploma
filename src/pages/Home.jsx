7c2c8585fde0bf17e2881bc544270e18750190e1
medina <damirovamedina38@gmail.com>
2023-06-09 18:46:12 +0300

    added and commented out some codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,19 @@ import "./Home.css";
 <Home></Home>;
 export default function Home() {
   return (
-    <div className="Home">
-      <h1>Welcome to my Perfume shop</h1>
-      {/*<video autoPlay loop>
-        <source src="https://player.vimeo.com/external/374819500.sd.mp4?s=3ff467c214bfb8caf9b6a37c98f4925486273fda&profile_id=164&oauth2_token_id=57447761" />
-      </video>*/}
-      <div className="container"> 
-       <div className="row">
-         <div className="col-6">
-          <img src="https://i.pinimg.com/474x/ae/02/26/ae02261d72d5c410c0c61e88ca9db0ac.jpg" alt="" />
-         <img src="https://i.pinimg.com/564x/a7/ef/01/a7ef01dc69f0dcc6a869ff90f59f7feb.jpg" alt="" />
-         <img src="https://i.pinimg.com/564x/bb/9c/88/bb9c88668913d817290eb0fde30b1514.jpg" alt="" />
-        </div>
-       </div>
-      </div>
+  //    {/*<video autoPlay loop>
+  //      <source src="https://player.vimeo.com/external/374819500.sd.mp4?s=3ff467c214bfb8caf9b6a37c98f4925486273fda&profile_id=164&oauth2_token_id=57447761" />
+  //</video>*/}
+  //    
+
+
+  <div className="Home">
+     <h1>Welcome to my Perfume shop</h1>
+     {/*<img src="https://i.pinimg.com/564x/a7/ef/01/a7ef01dc69f0dcc6a869ff90f59f7feb.jpg" alt="" />*/}
+     <img src="https://i.pinimg.com/564x/02/4c/a4/024ca4cf80aa1b0014920d65d2372519.jpg"></img>
+     <img src="https://i.pinimg.com/564x/eb/06/6c/eb066c0c5adf9150480806c5de7c43c2.jpg"></img>
+    
+    {/*<img src="https://i.pinimg.com/564x/bb/9c/88/bb9c88668913d817290eb0fde30b1514.jpg"></img>*/}
       <section className="page-section cta">
         <div className="container">
           <div className="row">
@@ -38,6 +37,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-    </div>
+      </div>
   );
 }
