b009c8201e9e884fc92967805c82c0e74b5638ca
medina <damirovamedina38@gmail.com>
2023-06-09 18:47:12 +0300

    commented out some codes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,17 @@ export default function About() {
 
       <div className="image"></div>
       <div className="about">
-        {/*<img src="https://i.pinimg.com/564x/80/8d/7f/808d7f8af18f2b61bcc6e7267a8c55d7.jpg"></img>*/}
+        <img src="https://i.pinimg.com/564x/13/77/c5/1377c56ee18f20aea47079792a0ec8bf.jpg"></img>
       </div>
 
       <div className="row">
         <div className="col-6">
           <div className="about">
-            {/*<img src="https://i.pinimg.com/236x/1b/92/e1/1b92e1420ff012479a458810493739cd.jpg"></img>*/}
+            <img src="https://i.pinimg.com/236x/1b/92/e1/1b92e1420ff012479a458810493739cd.jpg"></img>
             <img src="https://i.pinimg.com/564x/80/8d/7f/808d7f8af18f2b61bcc6e7267a8c55d7.jpg"></img>
+            {/*<img src="https://i.pinimg.com/564x/46/22/96/4622964e4a24c50758baed73e0ff8f96.jpg"></img>
+            <img src="https://i.pinimg.com/564x/ad/b6/0e/adb60e01f49f3aa49ac89699309995f5.jpg"></img>
+            <img src="https://i.pinimg.com/564x/f1/bc/7b/f1bc7b4d2013de5350d249474a0a0125.jpg"></img>*/}
           </div>
           <div className="col-6">
             <p>
@@ -24,7 +27,7 @@ export default function About() {
               benzoin and white musk.
             </p>
         </div>
-            <img src="https://i.pinimg.com/564x/13/77/c5/1377c56ee18f20aea47079792a0ec8bf.jpg"></img>
+        
           </div>
         </div>
       </div>
